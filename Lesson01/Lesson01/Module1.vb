77be279ea8219ad2ed054c6a127fb3c9f4d49a14
0x53olution <x53olution@icloud.com>
2014-10-15 15:22:24 +0200

    Lesson01

---
+++ Lesson01/Lesson01/Module1.vb
@@ -0,0 +1,63 @@
+﻿'Entweder in jeder Klasse/Modul deklarieren oder im Projekt selber
+Option Strict On 'Schränkt die Datentykonvertierung ein http://msdn.microsoft.com/de-de/library/zcd4xwzs.aspx
+Option Explicit On 'Erzwingt die explizite Variablendeklaration http://msdn.microsoft.com/de-de/library/y9341s4f.aspx
+
+Module Module1
+
+    'Main Methode
+    'Jede Konsolenanwendung hat einen Einstiegspunkt. Die "Main" Methode.
+    Sub Main()
+        'Aufruf der Methode Datentypen
+        'Datentypen()
+
+        WeitereWichtigeDatentypen()
+    End Sub
+
+    Private Sub Datentypen()
+        'Deklarieren einer Variablen
+        Dim maxInt32 As Integer
+
+        'Zuweisung eines Wertes
+        maxInt32 = Int32.MaxValue
+
+        'Das ganze kann auch in einem Schritt erledigt werden
+        Dim minInt32 As Integer = Int32.MinValue
+
+        'Ausgabe auf der Konsole mit WriteLine. ".WriteLine" beinhaltet gegenüber ".Write" einen Zeilenumbruch am Ende der Zeile
+        'Ausgabe in Textform wird immer in Anführungszeichen geschrieben.
+        'Um Ausdücke zu verknüpfen wir der "&" Operator verwendet
+        Console.WriteLine("Eine Integer Variable kann Zahlen von : " & minInt32 & " bis: " & maxInt32 & " annehmen.")
+
+        'Beispiele für Zahlen
+        Dim ganzeZahl As Integer = -123 'alle Ganzzahlen (positiv und negativ)
+        Dim natuelicheZahlen As UInteger = 123 'alle positiven Ganzzahlen
+        Dim rationaleZahlen As Double = -12.456 'alle rationalen Zahlen
+
+        'Beispiele für Zeichenketten bzw. einzelne Zeichen
+        Dim zeichenkette As String = "In einem String werden Unicodezeichen gespeichert"
+        Dim zeichen As Char = "A"c 'Das kleine "c" dienst als Literaltypzeichen.
+
+        'Beispiel für Wahrheitswerte
+        Dim wahrheitswert As Boolean = True
+
+        'Beispiel für einen beliebigen Datentyp
+        'Da alle Datentypen von object abgeleitet sind kann object alles annehmen
+        Dim ertsesObject = "Ein String in einem Objekt"
+        Dim zweitesObject = 123
+
+        'Ausgabe
+        'Bevor die Ausgabe ausgegeben wird, wird die Zeile umgebrochen
+        Console.WriteLine(Environment.NewLine & "Integer: " & ganzeZahl)
+        'Alternativ kann auch Console.Write genutzt werden und der Zeilenumbruch manuelle ingefügt werden
+        Console.Write("UInteger: " & natuelicheZahlen & Environment.NewLine)
+        Console.WriteLine("Double: " & rationaleZahlen)
+        Console.WriteLine("String: " & zeichenkette)
+        Console.WriteLine("Char: " & zeichen)
+        Console.WriteLine("Wahrheitswert: " & wahrheitswert)
+
+        'Es soll nicht wirklich etwas eingelesen werden.
+        'Das Konsolenfenster schließt sich aller dings nach der Abarbeitung. So wartet es auf eine Benutzereingabe.
+        Console.Read()
+    End Sub
+
+End Module
---
+++ Lesson01/Lesson01/Module2.vb
@@ -0,0 +1,51 @@
+﻿Option Strict On
+Option Explicit On
+
+Module Module2
+
+    Public Sub WeitereWichtigeDatentypen()
+        'Arrays (Felder) http://msdn.microsoft.com/de-de/library/wak0wfyt.aspx
+        'Eindimensional
+        Dim baureihen(5) As String 'Arrays beginnen bei 0
+        baureihen(0) = "F10"
+        baureihen(1) = "F11"
+        baureihen(2) = "F12"
+        baureihen(3) = "F01"
+        baureihen(4) = "F02"
+        baureihen(5) = "F03"
+        Console.WriteLine("5er ist die Baureihe: " & baureihen(0))
+
+        'Deklaration mit Anfangswerten. Die Grösse wird automatisch bestimmt
+        Dim baureihe5er As String() = {"F10", "F11"}
+        Console.WriteLine("5er Limusine: " & baureihe5er(0) & " ;5er Touring: " & baureihe5er(1))
+
+        'Mehrdimensional
+        Dim werkBaureihe(1, 1) As String
+        werkBaureihe(0, 0) = "Dingolfing"
+        werkBaureihe(0, 1) = "F10"
+        werkBaureihe(1, 0) = "München"
+        werkBaureihe(1, 1) = "F30"
+        Console.WriteLine("Der 5er wird in " & werkBaureihe(0, 0) & " gebaut. Der 3er wird in " & werkBaureihe(1, 0) & " gebaut.")
+
+        'Dictionary (unsortiert)
+        Dim bestellungen As New Dictionary(Of String, Integer)
+        'Das Dictionary besteht aus Schlüssel und Element
+        bestellungen.Add("F10", 5)
+        bestellungen.Add("F11", 8)
+
+        If bestellungen.ContainsKey("F10") Then
+            Console.WriteLine("Anzahl der Bestellungen für den F10: " & bestellungen.Item("F10"))
+        End If
+
+        'Liste
+        Dim meineAutoListe As New List(Of String)
+        meineAutoListe.Add("Bmw")
+        meineAutoListe.Add("Volkswagen")
+
+        Dim anzahlInMeinerAutoListe As Integer = meineAutoListe.Count
+        Console.WriteLine("In meiner Autoliste sind " & meineAutoListe.Count & " (" & meineAutoListe.Item(0) & ", " & meineAutoListe.Item(1) & ")")
+
+        Console.Read()
+    End Sub
+
+End Module
