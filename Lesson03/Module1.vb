fd41ab50f714c2338e81c1a85b8c31b36b6173af
0x53olution <x53olution@icloud.com>
2014-10-24 10:28:57 +0200

    Lesson03 erweitert

--- Lesson03/Module1.vb
+++ Lesson03/Module1.vb
@@ -4,6 +4,23 @@ Option Explicit On
 Module Module1
 
     Sub Main()
+        'Klasse instanziieren
+        'Objektvariable Mitarbeiter as Typ der zu instanziierenden Klasse definieren.
+        'Sie dient zum Zugriff auf die Elemente der Klasse.
+        Dim Mitarbeiter As Person
+        'Mit dem Schlüsselwort New wird Speicherplatz für eine Instanz der Klasse auf dem Managed Heap reserviert.
+        Mitarbeiter = New Person
+
+        'oder das ganze in einem Schritt
+        Dim Angestellter As New Person
+
+        'Mit der Objektinstanz auf die Klassenmember zugreifen
+        Mitarbeiter.nachname = "Gates"
+        Mitarbeiter.vorname = "Bill"
+
+        'Da die Klasse Person zweimal instanziiert wurde kann mit der zweiten Objektinstanz unabhänig von der ersten ein anderer Kontext verwendet werden.
+        Angestellter.nachname = "Ballmer"
+        Angestellter.vorname = "Steve"
 
     End Sub
 
---
+++ Lesson03/Person.vb
@@ -0,0 +1,14 @@
+﻿Option Strict On
+Option Explicit On
+
+'Ein Klasse besteht aus Membervariablen die eine logische Einheit bilden.
+Public Class Person
+
+    'Klassen bestehen aus Membervariablen und Runmpfdefinitionen die die Membervariablen einschliessen
+    'und der Klasse ihren Namen verleihen
+    Public nachname As String
+    Public vorname As String
+    Public groesse As Integer
+    Public alter As Integer
+
+End Class
